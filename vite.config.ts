b6d9e078b25f220b400db37a7e23ee24f13539ad
Vladimir Yazykov <neizerth@gmail.com>
2026-04-23 07:20:27 +0300

    feat: favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
+import favicons from "@peterek/vite-plugin-favicons";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // GitHub project page: https://<owner>.github.io/<repo>/
 const repoName = process.env.GITHUB_REPOSITORY?.split("/").at(-1);
-const base =
-  process.env.GITHUB_ACTIONS === "true" && repoName ? `/${repoName}/` : "/";
+const base = process.env.GITHUB_ACTIONS === "true" && repoName ? `/${repoName}/` : "/";
 
 export default defineConfig({
   base,
-  plugins: [react()],
+  plugins: [react(), favicons("src/assets/favicon.svg")],
   resolve: {
     alias: {
       "@app": "/src/slices/app",
